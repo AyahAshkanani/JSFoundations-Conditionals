4a73f3f65d034120de5a48af73a9596f6667063b
Ayah Ahmad <ayah.ash62@gmail.com>
2021-05-24 19:08:35 +0300

    solved the conditional tak

--- calculator.js
+++ calculator.js
@@ -1,6 +1,22 @@
 const prompt = require("prompt-sync")(); // Please don't touch me :)
-
+/*
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+*/
+const number1 = prompt(" enter first number: ");
+const number2 = prompt(" enter second number: ");
+const operation = prompt("operations are (+,-,*,/) choose one:");
+
+
+if(operation === "+") {console.log(`${number1} + ${number2} = ${parseInt(number1) + parseInt(number2)}`);}
+
+else if(operation === "-") {console.log(`${number1} - ${number2} = ${parseInt(number1) - parseInt(number2)}`);}
+
+else if(operation === "*") {console.log(`${number1} * ${number2} = ${parseInt(number1) * parseInt(number2)}`);}
+
+else if(operation === "/") {console.log(`${number1} / ${number2} = ${parseInt(number1) / parseInt(number2)}`);}
+
+else { console.log("wrong operation chosen");}
+
